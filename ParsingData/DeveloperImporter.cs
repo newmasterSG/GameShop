dd7e7613314b22e5bc4f779ace3daacca9810b41
Yehor <kadeniuk@sunpoint.com.ua>
2023-08-02 18:34:46 +0300

    Adding distinct object

---
+++ ParsingData/DeveloperImporter.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Models.Developer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingData
+{
+    public class DeveloperImporter
+    {
+        public static async Task<DevelopersModel> CreateDeveloper(DevelopersModel developer)
+        {
+            DevelopersModel developersModel = new DevelopersModel()
+            {
+                Slug = developer.Slug,
+                Name = developer.Name,
+            };
+
+            return developersModel;
+        }
+    }
+}
--- ParsingData/Program.cs
+++ ParsingData/Program.cs
@@ -8,9 +8,11 @@ using Infrastructure.Models.Platform;
 using Infrastructure.Models.Rating;
 using Infrastructure.Models.ShortScreenshot;
 using Infrastructure.Models.Store;
+using Infrastructure.Models.StoreInfo;
 using Infrastructure.Models.Tags;
 using Infrastructure.Repository.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Newtonsoft.Json;
 using ParsingData.Requests;
 using ServicesLayer.Services.Interfaces;
@@ -25,25 +27,50 @@ namespace ParsingData
         static async Task Main(string[] args)
         {
             var client = new HttpClient();
-
-            //GetRequest devgetRequest = new GetRequest(client, "https://api.rawg.io/api/developers?key=e5be80adcd7348d9bfe0c55a970b5215&page=");
-            //var developerList = await new Deserializer<DevelopersModel>().Deserialize(await devgetRequest.GetJson(4));
             List<Task<RootObject<GamesModel>>> task = new List<Task<RootObject<GamesModel>>>();
-
+            string apiKey = "e5be80adcd7348d9bfe0c55a970b5215";
+            //GetRequest devRequest = new GetRequest(client, "https://api.rawg.io/api/developers?key=e5be80adcd7348d9bfe0c55a970b5215&page=");
+            string gamesUrl = "https://api.rawg.io/api/games?key={0}&developers={1}&page=";
+            //var storeList = await new Deserializer<TagModel>().Deserialize(await games.GetJson(4));
+            //List<Task<RootObject<DevelopersModel>>> task = new List<Task<RootObject<DevelopersModel>>>();
             using (GameShopContext gameShopContext = new GameShopContext())
             {
                 Services<GamesModel> gamesModelService = new Services<GamesModel>(new Repository<GamesModel>(gameShopContext));
                 Services<DevelopersModel> developerServices = new Services<DevelopersModel>(new Repository<DevelopersModel>(gameShopContext));
                 GamesModel gam = new GamesModel();
-                var existingDeveloper = await developerServices.GetAllAsync();
-                foreach (var developer in existingDeveloper)
+                GameImporter gameImporter = new GameImporter(gameShopContext);
+
+                //for (int i = 1; i <= 3; i++)
+                //{
+                //    task.Add(new Deserializer<DevelopersModel>().Deserialize(await devRequest.GetJson(i)));
+                //}
+
+                //var results = await Task.WhenAll(task);
+
+                //foreach (var result in results)
+                //{
+                //    foreach (var item in result.Results)
+                //    {
+                //        DevelopersModel model = new DevelopersModel()
+                //        {
+                //            Name = item.Name,
+                //            Slug = item.Slug,
+                //        };
+
+                //        await developerServices.AddAsync(model);
+                //    }
+                //}
+
+
+                var existDev = await developerServices.GetAllAsync();
+                var exist = existDev.TakeLast(2);
+                foreach (var dev in exist)
                 {
-                    var existDeveloper = gameShopContext.Developers.FirstOrDefault(i => i.Name == developer.Name);
-                    string Url = $"https://api.rawg.io/api/games?key=e5be80adcd7348d9bfe0c55a970b5215&developers={developer.Slug}&page=";
-                    GetRequest gameGetRequest = new GetRequest(client, Url);
-                    for (int i = 1; i <= 2; i++)
+                    var url = string.Format(gamesUrl, apiKey, dev.Slug, "{0}");
+                    GetRequest games = new GetRequest(client, url);
+                    for (int i = 1; i <= 1; i++)
                     {
-                        task.Add(new Deserializer<GamesModel>().Deserialize(await gameGetRequest.GetJson(i)));
+                        task.Add(new Deserializer<GamesModel>().Deserialize(await games.GetJson(i)));
                     }
 
                     var results = await Task.WhenAll(task);
@@ -52,19 +79,12 @@ namespace ParsingData
                     {
                         foreach (var item in result.Results)
                         {
-                            gam = await GameImporter.CreateGame(item, developer);
+                            gam = await gameImporter.CreateGame(item, dev);
                             await gamesModelService.AddAsync(gam);
-                            //var existingGame = gameShopContext.Games.FirstOrDefault(i => i.Name == gam.Name);
-                            //if (existingGame == null)
-                            //{
-                            //    await gamesModelService.AddAsync(gam);
-                            //}
-                            //else
-                            //{
-                            //}
                         }
                     }
                 }
+
             }
         }
     }
