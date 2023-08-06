adf3129459d99f3d489dc351cdd0556a014f1525
Yehor <kadeniuk@sunpoint.com.ua>
2023-08-06 17:03:02 +0300

    Adding typscript

---
+++ UI/Scripts/start.js
@@ -0,0 +1,8 @@
+var myNavBar = document.getElementById('myNavBar');
+myNavBar === null || myNavBar === void 0 ? void 0 : myNavBar.addEventListener('mouseover', function () {
+    myNavBar.style.animation = 'waveEffect 2s linear infinite';
+});
+myNavBar === null || myNavBar === void 0 ? void 0 : myNavBar.addEventListener('mouseleave', function () {
+    myNavBar.style.animation = 'none';
+});
+//# sourceMappingURL=start.js.map
